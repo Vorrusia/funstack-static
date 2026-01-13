e7556d196336aa20d620a35d11beb0509fae6211
uhyo <uhyo@uhy.ooo>
2026-01-13 23:16:00 +0900

    docs: update docs package to use FUNSTACK Router without errors

--- packages/docs/src/App.tsx
+++ packages/docs/src/App.tsx
@@ -1,3 +1,14 @@
+import { Router } from "@funstack/router";
+import { route, type RouteDefinition } from "@funstack/router/server";
+import { Home } from "./pages/Home";
+
+const routes: RouteDefinition[] = [
+  route({
+    path: "/",
+    component: <Home />,
+  }),
+];
+
 export default function App() {
-  return <div>Docs App</div>;
+  return <Router routes={routes} />;
 }
---
+++ packages/docs/src/pages/Home.tsx
@@ -0,0 +1,8 @@
+export const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to FUNSTACK Static Docs</h1>
+      <p>This is the home page of the FUNSTACK Static documentation site.</p>
+    </div>
+  );
+};
